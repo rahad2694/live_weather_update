edb81599217a3ad7345e20dbe12ee00db22f7dce
rahad2694 <rahad.haw@gmail.com>
2022-03-04 02:37:37 +0600

    Search button functonal

--- js/app.js
+++ js/app.js
@@ -6,3 +6,11 @@ const weatherCondition = document.getElementById('weather-condition');
 const temperature = document.getElementById('temperature-value');
 const errorMessage = document.getElementById('error-message');
 const resultArea = document.getElementById('result-area');
+const searchBtn = () =>{
+    errorMessage.innerText='';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputBox.value}&appid=${API_Key}&units=metric`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayResult(data));
+    inputBox.value='';
+}
